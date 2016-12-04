3043b45e686ba4d88a661ca39a5efb6ec6126323
tawshif <anikphp@gmail.com>
2016-12-05 00:40:40 +0600

    changes

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+var del = document.getElementsByClassName('delete')
+del.addEventListener("click", function() {
+    console.log(del);
+    var conf = confirm("are u sure ");
+    if (conf) {
+
+    }
+
+});
